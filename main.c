ea42ac5b65cb5b202698943672492209fff0bab6
Hoby Ratefy <hoby.ratefy@protonmail.com>
2023-04-06 00:51:12 +0900

    Refactor functions

--- main.c
+++ main.c
@@ -2,12 +2,41 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+// Display the score in a readable format.
 void display_score(int *players_scores) {
   printf("%d - %d\n", players_scores[0], players_scores[1]);
 }
 
+// Display the name of a player
 void display_name(char *player_name) {
   printf("%s\n", player_name);
+}
+
+// Modify the score of a player
+int add_point_player(int player_score)
+{
+  int new_score;
+
+  if (player_score == 0) {
+    new_score = 15;  
+  } else if (player_score == 15) {
+    new_score = 30;
+  } else if (player_score == 30) {
+    new_score = 40;
+  } else if (player_score == 40) {
+    new_score = 60;
+  }
+  return new_score;
+}
+
+// Check for deuce situation in a game
+void check_deuce(int *players_scores, bool *is_deuce)
+{
+  if (players_scores[0] == 40 && players_scores[1] == 40) {
+    *is_deuce = true;
+  } else {
+    *is_deuce = false;
+  }
 }
 
 int main(int argc, char *argv[])
@@ -19,9 +48,10 @@ int main(int argc, char *argv[])
 
   int players_games[2] = {0, 0};
 
-  int players_score[2] = {0, 15};
+  int players_score[2] = {0, 0};
 
   bool is_deuce = false;
+  bool is_advantage = false;
   bool is_match_over = false;
 
   printf("Please start the match.\n");
@@ -41,43 +71,30 @@ int main(int argc, char *argv[])
   */
 
   display_score(players_score);
-  display_name(player_2_name);
+  players_score[0] = add_point_player(players_score[0]);
+  display_score(players_score);
+  check_deuce(players_score, &is_deuce);
+  printf("Deuce? %d\n", is_deuce);
+  players_score[0] = add_point_player(players_score[0]);
+  display_score(players_score);
+  check_deuce(players_score, &is_deuce);
+  printf("Deuce? %d\n", is_deuce);
+  players_score[1] = add_point_player(players_score[1]);
+  display_score(players_score);
+  check_deuce(players_score, &is_deuce);
+  printf("Deuce? %d\n", is_deuce);
+  players_score[1] = add_point_player(players_score[1]);
+  display_score(players_score);
+  check_deuce(players_score, &is_deuce);
+  printf("Deuce? %d\n", is_deuce);
+  players_score[0] = add_point_player(players_score[0]);
+  display_score(players_score);
+  check_deuce(players_score, &is_deuce);
+  printf("Deuce? %d\n", is_deuce);
+  players_score[1] = add_point_player(players_score[1]);
+  display_score(players_score);
+  check_deuce(players_score, &is_deuce);
+  printf("Deuce? %d\n", is_deuce);
 
   return 0;
-}
-
-
-int add_point_player(int player_score, bool deuce)
-{
-  int new_score = 0;
-
-  if (player_score == 0) {
-    new_score = 15;  
-  } else if (player_score == 15) {
-    new_score = 30;
-  } else if (player_score == 30) {
-    new_score = 40;
-  } else if (player_score == 40) {
-    if (deuce) {
-      new_score = 45;
-    } else {
-      new_score = 60;
-    }
-  } else if (player_score == 45) {
-    if (deuce) {
-      new_score = 40;
-    } else {
-      new_score = 60;
-    }
-  }
-
-  return new_score;
-}
-
-bool check_deuce(int player_1_score, int player_2_score)
-{
-  if (player_1_score == 40 && player_2_score == 40) {
-    return true;
-  }
-  return false;
 }
